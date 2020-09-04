7736f49503802641d127ce68def41d2c5154889e
Kevin MARTINS <kemartin@e1r3p16.42.fr>
2020-09-04 17:11:23 +0200

    reading_csv added and inspire by agissing, predict added with the subject formula

--- pricebykm.py
+++ pricebykm.py
@@ -1,19 +1,34 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy import stats
+import csv
 
 def predict(theta0, theta1, km):
 	return theta0 + theta1 * km
-	
+
+def reading_csv():
+	try:
+		with open("data.csv", 'r') as csvfile:
+			file = csv.DictReader(csvfile)
+			km_grid = []
+			price_grid = []
+			for data in file:
+				km_grid.append(float(data['km']))
+				price_grid.append(float(data['price']))
+		return km_grid, price_grid
+	except:
+		print("error while reading csv!")
+		exit(1)
+
 if __name__ == "__main__":
-	df = pd.read_csv("data.csv")
-	km = df.iloc[0:len(df),0]
-	price = df.iloc[0:len(df),1] 
+	km, price = reading_csv()
 
 	inputkm = input("Le kilometrage de la voiture a estime :\n")
 	slope, intercept, r_value, p_value, std_err = stats.linregress(km, price)
 	print(predict(intercept, slope, inputkm))
-	axes = plt.axes()
-	axes.grid()
-	plt.scatter(km,price)
-	plt.show()
+
+
+	#axes = plt.axes()
+	#axes.grid()
+	#plt.scatter(km,price)
+	#plt.show()
