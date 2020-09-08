2b6966d8ec5b84604664c37eaf9f7ce8f7b62bbf
Kevin MARTINS <kemartin@e1r10p14.42.fr>
2020-09-08 20:05:48 +0200

    try to solve the learning alg

--- learningpricebykm.py
+++ learningpricebykm.py
@@ -34,18 +34,18 @@ def theta_csv(theta0, theta1):
 def predict(theta0, theta1, km):
 	return theta0 + theta1 * km
 
-def learning_thetas(X, Y):
+def learning_thetas(X, Y, theta0, theta1):
 	tmptheta0 = 0
 	tmptheta1 = 0
-	scale = 1
+	Lrate = 10000
 	B0 = 0
 	B1 = 0
 	m = len(X)
 	for i in range(len(X)):
-		B0 += scale * 1/m * (X[i] - Y[i])
-		B1 += scale * 1/m * (X[i] - Y[i]) * X[i]
-	tmptheta0 = scale * 1/m * B0
-	tmptheta1 = scale * 1/m * B1
+		B0 += (predict(theta0, theta1, X[i]) - Y[i])
+		B1 += (predict(theta0, theta1, X[i]) - Y[i]) * X[i]
+	tmptheta0 = Lrate * 1.0/m * B0
+	tmptheta1 = Lrate * 1.0/m * B1
 	return tmptheta0, tmptheta1
 
 if __name__ == "__main__":
@@ -57,17 +57,18 @@ if __name__ == "__main__":
 		except:
 			print("Error with the data.csv file!")
 			exit (1)
-		learning_thetas(X, Y)
-		# Xmean= np.mean(X)
-		# Ymean= np.mean(Y)
-		# B1Up = 0
-		# B1Down = 0
-		# for i in range(len(X)):
-		# 	B1Up += (X[i] - Xmean) * (Y[i] - Ymean)
-		# 	B1Down += (X[i]-Xmean) ** 2
-		# theta1 = B1Up / B1Down
-		# theta0 = Ymean - theta1 * Xmean
-		theta0, theta1 = learning_thetas(X, Y)
+		Xmean= np.mean(X)
+		Ymean= np.mean(Y)
+		B1Up = 0
+		B1Down = 0
+		for i in range(len(X)):
+			B1Up += (X[i] - Xmean) * (Y[i] - Ymean)
+			B1Down += (X[i]-Xmean) ** 2
+		theta1 = B1Up / B1Down
+		theta0 = Ymean - theta1 * Xmean
+		print (str(theta0) + " | " + str(theta1))
+		theta0, theta1 = learning_thetas(X, Y, theta0, theta1)
+		print (str(theta0) + " | " + str(theta1))
 		line = theta0 + theta1 * X
 		theta_csv(theta0, theta1)
 		predicted = predict(theta0, theta1, int(options.mileage))
